08cf33f3042afe3e8419f23f5f0aaccfc42c57de
fernandojsmelo <fernandojsmelo@gmail.com>
2024-02-27 08:53:02 -0300

    Configurando ThemeProvider do Styles-Components

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import ReactDOM from 'react-dom/client';
+
 import Home from './templates/App';
 import { GlobalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
