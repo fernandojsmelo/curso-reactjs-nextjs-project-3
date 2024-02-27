8b76de11d508c81dcb9a56be64549ea2878b70f8
fernandojsmelo <fernandojsmelo@gmail.com>
2024-02-27 09:33:56 -0300

    Configurando jest e Jest-Styles-Components

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import Home from '.';
+import { renderTheme } from '../../styles/render-therme';
+import { theme } from '../../styles/theme';
 
 test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { debug } = renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', {
+    name: 'Hello',
+  }).parentElement;
+  expect(headingContainer).toHaveStyle({
+    background: theme.colors.secondaryBg,
+  });
+  expect(headingContainer).toMatchSnapshot();
+  //expect(headingContainer).toHaveStyleRule('background', 'red');
 });
