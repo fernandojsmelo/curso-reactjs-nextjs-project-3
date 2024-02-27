a757c29c0bfa4c83d7ac2b080183134912bfc672
fernandojsmelo <fernandojsmelo@gmail.com>
2024-02-27 11:10:31 -0300

    Configurando o Storybook - Parte 1

---
+++ .storybook/preview.js
@@ -0,0 +1,28 @@
+import { Story } from '@storybook/blocks';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../src/styles/global-styles';
+import { theme } from '../src/styles/theme';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <GlobalStyles />
+    </ThemeProvider>
+  ),
+];
